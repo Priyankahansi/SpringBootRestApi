6d8a31d936b5ee5d6875b137a3cca52a0ad48aca
Priyanka <priyankahansi95@gmail.com>
2022-06-25 13:11:04 +0530

    added:Name as Query

--- src/main/java/com/bridglebabz/controller/SpringController.java
+++ src/main/java/com/bridglebabz/controller/SpringController.java
@@ -1,21 +1,29 @@
 package com.bridglebabz.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SpringController {
 
-    @RequestMapping(value = ("/hello"),method = RequestMethod.GET)
-    public String hello()
-    {
+    @RequestMapping(value = ("/hello"), method = RequestMethod.GET)
+    public String hello() {
+
         return "Hello Everyone from Bridgelabz";
     }
+
     @GetMapping("/world")
-    public String world(){
+    public String world() {
 
         return "hello world";
+    }
+
+    @PostMapping("/send/{name}")
+    public String post(@PathVariable String name) {
+        return "hello" + " " + name + " " + "..!";
+    }
+
+    @PostMapping("/query")
+    public String share(@RequestParam String name) {
+        return "hello" + " " + name + " " + "..!";
     }
 }
